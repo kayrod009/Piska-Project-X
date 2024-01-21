241964d6ac43d75e62049b8cb8ea5cd1d15f207b
Asus <kidepdone@gmail.com>
2024-01-21 17:13:36 +0330

    user class needed parts

--- main.py
+++ main.py
@@ -14,3 +14,35 @@ class Signup:
             # peyman = Patient("test@test", "patient", 1234)
         elif role == "staff":
             globals()[f"{username}"] = Staff(username, email, password)
+
+
+class User:
+    staff = dict()
+    patient = dict()
+
+    def __init__(self, username):
+        self.username = username
+
+    @staticmethod
+    def sign_in(username, role):
+        global status, current_user
+        pass
+
+    @staticmethod
+    def make_instance(username, role):
+        pass
+
+    @staticmethod
+    def sign_out():
+        pass
+
+    @staticmethod
+    def back():
+        pass
+
+    @staticmethod
+    def options():
+        pass
+
+    def showmenu(self):
+        pass
