4bc94b94b41fa76095966b74638b90c0949f3a21
setayesh <stheydari10@gmaill.com>
2024-01-21 17:08:31 +0330

    sign up class

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ class Notification(ABC):
     def confirm():
         print("Done successfully")
 
+class Signup:
+
+    def __init__(self, username, email, role, password):
+
+        if role == "patient":
+            globals()[f"{username}"] = Patient(username, email, password)
+            # peyman = Patient("test@test", "patient", 1234)
+        elif role == "staff":
+            globals()[f"{username}"] = Staff(username, email, password)
