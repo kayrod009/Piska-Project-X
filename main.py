7bdf167d6bc54c4c456f465e205c487e267111e5
setayesh <stheydari10@gmaill.com>
2024-01-21 17:19:11 +0330

    staff update 2

--- main.py
+++ main.py
@@ -6,14 +6,47 @@ class Notification(ABC):
         print("Done successfully")
 
 class Signup:
-
-    def __init__(self, username, email, role, password):
-
+    @staticmethod
+    def sign_up(username, email, role, password):
         if role == "patient":
             globals()[f"{username}"] = Patient(username, email, password)
             # peyman = Patient("test@test", "patient", 1234)
         elif role == "staff":
             globals()[f"{username}"] = Staff(username, email, password)
+
+class Patient(Notification, User):
+    def current_reserved(self):
+        #get from database
+        pass
+    def history(self):
+        #get from database
+        pass
+    def new_reservation(self):
+        #post to database
+        pass
+
+class Staff(Notification, User):
+    menu = "1.reserved appointments \n2.cancel appointment \n3.increase capacity"
+
+    def current_reserved(self):
+        self.stat_index = "11"
+        # get from database
+        pass
+    def cancel(self):
+        appo_id = int(input("enter the appointment's id"))
+        self.stat_index = "12"
+        self.confirm()
+        # get from database
+        pass
+
+
+
+# class Doctor:
+#     pass
+
+
+class Appoinment:
+    pass
 
 
 class User:
