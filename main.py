94a345e4da5a59281c358357fcb30cd4021b29c1
Asus <kidepdone@gmail.com>
2024-01-22 18:17:15 +0330

    import pyotp

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from abc import ABC
 import pyotp
+import sqlite3
+
+connection = sqlite3.connect("PiskaDB.db")
+cursor = connection.cursor()
+
 
 class Notification(ABC):
     @staticmethod
     def confirm():
         print("Done successfully")
+
 
 class Signup:
     @staticmethod
@@ -15,6 +21,7 @@ class Signup:
         elif role == "staff":
             globals()[f"{username}"] = Staff(username, email, password)
 
+
 class Patient(Notification, User):
     menu = "1.reserved appointments \n2.history \n3.new reservation"
 
@@ -22,6 +29,7 @@ class Patient(Notification, User):
         self.stat_index = "11"
         # get from database
         pass
+
     def history(self):
         self.stat_index = "12"
         # get from database
@@ -34,6 +42,7 @@ class Patient(Notification, User):
         get_id = int(input("enter your doctor/clinic id"))
         globals()[f"{self.username}"] = Appoinment(get_id, self.id)
 
+
 class Staff(Notification, User):
     menu = "1.reserved appointments \n2.cancel appointment \n3.increase capacity"
 
@@ -41,22 +50,19 @@ class Staff(Notification, User):
         self.stat_index = "11"
         # get from database
         pass
+
     def cancel(self):
         appo_id = int(input("enter the appointment's id"))
         self.stat_index = "12"
         self.confirm()
         # get from database
         pass
+
     def increase_cap(self):
         self.stat_index = "13"
         self.confirm()
         # post to database
         pass
-
-
-
-# class Doctor:
-#     pass
 
 
 class Appoinment:
@@ -73,7 +79,39 @@ class User:
     @staticmethod
     def sign_in(username, role):
         global status, current_user
-        pass
+        if role == "patient":
+            cursor.execute("SELECT password FROM patients WHERE username = ? ", (username,))
+            usr_pas = cursor.fetchone()
+        elif role == "staff":
+            cursor.execute("SELECT password FROM staffs WHERE username = ? ", (username,))
+            usr_pas = cursor.fetchone()
+        else:
+            print("your role must be staff or patient!")
+            return
+        if usr_pas is None:
+            print("please sign up first!")
+        else:
+            passtype = input("password or OTP:")
+            if passtype == "password":
+                ask_pass = input("please enter your password: ")
+                if ask_pass == usr_pas[0]:  # sign in successfully
+                    User.make_instance(username, role)
+                    status = "10"
+                    current_user = username
+                else:
+                    print("Wrong password !")
+            elif passtype.lower() == "otp":
+                totp = pyotp.TOTP('base32secret3232')
+                otp = totp.now()
+                print(otp)
+                ask_otp = input("please enter your OTP: ")
+                if otp == ask_otp:  # sign in successfully
+                    User.make_instance(username, role)
+                    status = "10"
+                    current_user = username
+                    del otp
+                else:
+                    print("otp didn't match!")
 
     @staticmethod
     def make_instance(username, role):
