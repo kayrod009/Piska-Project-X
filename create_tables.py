6158136d2bbf0ec7c7d9828f59b2fa592e08c005
nimabaghernejad <nimabaghernejad106@gmail.com>
2024-01-22 18:02:18 +0330

    Creating patients table

--- create_tables.py
+++ create_tables.py
@@ -11,3 +11,13 @@ CREATE TABLE IF NOT EXISTS patients (
 );
 ''')
 connection.commit()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS staffs (
+    staff_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+''')
+connection.commit()
