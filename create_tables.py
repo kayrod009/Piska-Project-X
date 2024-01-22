f78ce3c92660d2789df1399bb6284b3dfe301dcf
nimabaghernejad <nimabaghernejad106@gmail.com>
2024-01-22 18:02:03 +0330

    Creating patients table

--- create_tables.py
+++ create_tables.py
@@ -1,1 +1,13 @@
+import sqlite3
 
+connection = sqlite3.connect('PiskaDB.db')
+cursor = connection.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS patients (
+    patient_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+''')
+connection.commit()
